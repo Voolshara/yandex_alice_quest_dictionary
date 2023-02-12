6fb09451ded94924f63e527674c34bad41514579
StanisLove <sta-tun-slav@yandex.ru>
2023-02-12 23:14:03 +0300

    w8 host

---
+++ yandex_alice_quest_dictionary/db/setup.py
@@ -0,0 +1,4 @@
+from yandex_alice_quest_dictionary.db.db import setup_base_data
+
+def run():
+    setup_base_data()
---
+++ yandex_alice_quest_dictionary/load_img.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import requests
+
+
+load_dotenv("yandex_alice_quest_dictionary/.env")
+
+with open("yandex_alice_quest_dictionary/assets/space.png", "r") as file:
+    pass
