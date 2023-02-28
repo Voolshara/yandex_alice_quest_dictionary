e914aaf28d62af9ea9400fd1f7f3a4840d85a1d8
StanisLove <sta-tun-slav@yandex.ru>
2023-03-01 02:02:59 +0300

    dfkpsfhmtpbpinvfipvnovdsoufbnd

--- yandex_alice_quest_dictionary/main.py
+++ yandex_alice_quest_dictionary/main.py
@@ -134,8 +134,8 @@ def handle_dialog(res,req):
         state_data["run_quest"] = state_data["run_quest"][0]
         state_data["state"] = state_data["state"][0]
 
-        res['response']["session_state"] = dict()
-        res['response']["session_state"]["value"] = state_data
+        res["session_state"] = state_data
+        # res["session_state"]["value"] = state_data
 
         pp(state_data)
         pp(res['response'])
