bc4ca637ff43b7ab04cc96f93d7574384810b73d
StanisLove <sta-tun-slav@yandex.ru>
2023-03-01 02:01:28 +0300

    fix bugs

--- yandex_alice_quest_dictionary/main.py
+++ yandex_alice_quest_dictionary/main.py
@@ -134,6 +134,7 @@ def handle_dialog(res,req):
         state_data["run_quest"] = state_data["run_quest"][0]
         state_data["state"] = state_data["state"][0]
 
+        res['response']["session_state"] = dict()
         res['response']["session_state"]["value"] = state_data
 
         pp(state_data)
