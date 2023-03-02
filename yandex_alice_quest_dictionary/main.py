42902397cb9a5e928c9866f76f2fc1915cb9a8e0
StanisLove <sta-tun-slav@yandex.ru>
2023-03-02 03:46:23 +0300

    try fix bug for not leaving from base

--- yandex_alice_quest_dictionary/main.py
+++ yandex_alice_quest_dictionary/main.py
@@ -41,7 +41,7 @@ class Alice_Worker:
 
         self.state = json_req["state"]["session"]
 
-        self.input_text = json_req['request']['original_utterance']
+        self.input_text = json_req['request']['command']
         
     def load_dict(self):
         now_quest = self.state["running_quest"]
@@ -123,12 +123,14 @@ def on_quest_state(Alice):
 
 
 def prepare_quest(Alice):
+    print(Alice.input_text in Alice.dicts)
     if Alice.input_text in Alice.dicts:
         Alice.response['response']['text'] = Alice.dicts_descriptions[Alice.input_text]
         Alice.set_quest_status()
         return
     elif Alice.input_text.lower == "что ты умеешь":
         Alice.response['response']['text'] = "Привет, я проведу для тебя квест. Выбери один из доступных. Ты так же можешь настроить ключевые слова под себя"
+        return
     no_understanding(Alice)
 
 
